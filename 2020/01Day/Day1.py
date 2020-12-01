201aaba3b875aa50d506512896d74848da7f71e2
Omkar Powar <omkar.powar@formlabs.com>
2020-12-01 00:54:54 -0500

    Day 1

--- 2020/01Day/Day1.py
+++ 2020/01Day/Day1.py
@@ -1,21 +1,32 @@
 import fileinput
+import time
+start_time = time.time()
 
-lines = list(fileinput.input())
+input_data = list(fileinput.input())
 
 def part1():
-	for line in lines:
+	for line in input_data:
 		rest = 2020 - int(line)
-		if rest in map(int,lines):
+		if rest in map(int,input_data):
 			return(int(rest)*int(line))
 
 def part2():
-	for line in lines:
-		rest = 2020 - int(line)
-		for i in lines:
-			rest2 = rest - int(i)
-			if rest2 in map(int, lines):
-				_mulitple = int(i)*int(line)
-				return(_mulitple*rest2)
+	minimum_val = min(input_data)
+	for line in input_data:
+		leftover = 2020 - int(line)
+		if leftover < int(minimum_val):
+			break
+		else:
+			for i in input_data:
+				leftover_sides = leftover - int(i)
+				if leftover_sides > 0:
+					if leftover_sides in map(int, input_data):
+						_mulitple = int(i)*int(line)
+						return(_mulitple*leftover_sides)
+				else:
+					break
 
 print(part1())
 print(part2())
+
+print("--- %s seconds ---" % (time.time() - start_time))
