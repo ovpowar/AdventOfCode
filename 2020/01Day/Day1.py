7ecd1c0c4f6ff4dfd1364c7c812add82308c6d98
Omkar Powar <omkar.powar@formlabs.com>
2020-12-01 09:31:17 -0500

    Day 1

--- 2020/01Day/Day1.py
+++ 2020/01Day/Day1.py
@@ -14,19 +14,15 @@ def part2():
 	minimum_val = min(input_data)
 	for line in input_data:
 		leftover = 2020 - int(line)
-		if leftover < int(minimum_val):
-			break
-		else:
+		if leftover > int(minimum_val):
 			for i in input_data:
 				leftover_sides = leftover - int(i)
 				if leftover_sides > 0:
 					if leftover_sides in map(int, input_data):
 						_mulitple = int(i)*int(line)
 						return(_mulitple*leftover_sides)
-				else:
-					break
 
 print(part1())
 print(part2())
 
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s milliseconds ---" % (float(time.time() - start_time)*1000))
